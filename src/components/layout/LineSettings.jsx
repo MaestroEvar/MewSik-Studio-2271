3484ffe0e2664500689110693ba33c7e5076787f
Zubroza <tumunbayarov400@mail.ru>
2026-06-03 21:53:55 +0800

    Редактирование блока Line Settings

--- src/components/layout/LineSettings.jsx
+++ src/components/layout/LineSettings.jsx
@@ -1,4 +1,10 @@
 import './LineSettings.css';
+import workInProgress from '../sprites/work_in_progress.png'; // Импортируем изображение
+
 export default function LineSettings() {
-    return <div className="app-linesettings">Line Settings opop</div>;
+    return <div className="app-linesettings">Line Settings 
+    
+    <p>Здесь будут настройки линий, которые можно будет сохранять в паттерны</p>
+    <img src={workInProgress} alt="Work in progress" />
+    </div>;
 }
