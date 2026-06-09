75b92ab702e81a63662c3591770856e1072705fd
Alexandr <sashok.ey@mail.ru>
2026-06-09 18:07:49 +0800

    Исправления деплоя

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',          // относительные пути
-  build: {
-    outDir: 'build'
-  }
+  base: '/MewSik-Studio-2271/',
 });
